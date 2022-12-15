c931f7f40ca57cc728703933a47ffd6c9f7e7f0b
DDEV-Local User <nobody@example.com>
2022-12-15 16:28:09 +0000

    created controller to create the artist admin config page

--- src/Controller/GetArtistController.php
+++ src/Controller/GetArtistController.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\cyber_duck_spotify_artists\Controller;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Component\Serialization\Json;
 
 /**
- * Class GetArtistController.
+ * This Class is used for reading Artist node entity.
  */
 class GetArtistController extends ControllerBase {
 
@@ -16,9 +18,49 @@ class GetArtistController extends ControllerBase {
    *   Return Hello string.
    */
   public function fetch() {
+    // Empty data array.
+    $artists = [];
+    $counter = 0;
+    // Get current user roles.
+    $roles = \Drupal::currentUser()->getRoles();
+
+    // Fetch all Artist contents.
+    $entities = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'artist']);
+
+    // Check if $entities variable is empty.
+    if (!empty($entities)) {
+      // Loop through each entity.
+      foreach ($entities as $entity) {
+        // Check if $entity variable is an entity.
+        if ($entity instanceof EntityInterface) {
+          // Check if entity has field_spotify_id & title fields.
+          if ($entity->get('field_spotify_id')->value && $entity->get('title')->value) {
+            // Populate array with data.
+            $artists['artists'][] = [
+              'spotify_id' => (string) $entity->get('field_spotify_id')->value,
+              'name' => (string) $entity->get('title')->value,
+            ];
+          }
+        }
+      }
+      $counter = count($artists['artists']);
+    }
+
     return [
-      '#type' => 'markup',
-      '#markup' => '<div id="spotify-artists"></div>'
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#cache' => [
+        'max-age' => 0,
+      ],
+      '#attributes' => [
+        'id' => 'spotify-artists',
+        'data-artists' => Json::encode($artists),
+        'data-roles' => Json::encode($roles),
+        'data-counter' => $counter,
+      ],
+      '#attached' => [
+        'library' => ['cyber_duck_spotify_artists/spotify_artists'],
+      ],
     ];
   }
 
